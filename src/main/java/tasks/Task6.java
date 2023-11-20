0715d3d21863168e7e51a917ef89ad55938fd63b
ministr-kaifa <eyzeep@yandex.ru>
2023-11-20 19:10:48 +0300

    task6: review ready

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -3,9 +3,9 @@ package tasks;
 import common.Area;
 import common.Person;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Имеются
@@ -19,6 +19,15 @@ public class Task6 {
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+    var areasNames = areas.stream()
+      .collect(Collectors.toMap(
+        Area::getId,
+        Area::getName
+      ));
+
+    return persons.stream()
+      .flatMap(person -> personAreaIds.get(person.getId()).stream()
+        .map(areaId -> person.getFirstName() + " - " + areasNames.get(areaId)))
+      .collect(Collectors.toSet());
   }
 }
